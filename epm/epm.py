18b850d4d43b032459ebb6b593a98c081055f43e
sam <cr0xxy94@gmail.com>
2020-02-03 18:57:56 +0200

    Add a main view widget

--- epm/epm.py
+++ epm/epm.py
@@ -6,10 +6,12 @@ from PySide2.QtCore import Slot
 from PySide2.QtWidgets import QApplication, QStackedWidget, QWidget
 
 from exercise import ExerciseWidget
+from mainwidget import MainWidget
 
 
 class MainWindow(QWidget):
     widget_stack = None
+    main_widget = None
     exercise_widget = None
 
     def __init__(self):
@@ -17,13 +19,17 @@ class MainWindow(QWidget):
 
         self.setup_window()
 
-        self.widget_stack.setCurrentWidget(self.exercise_widget)
+        self.widget_stack.setCurrentWidget(self.main_widget)
 
     def setup_window(self):
         self.widget_stack = QStackedWidget(self)
+        self.main_widget = MainWidget()
         self.exercise_widget = ExerciseWidget()
 
+        self.widget_stack.addWidget(self.main_widget)
         self.widget_stack.addWidget(self.exercise_widget)
+
+        self.main_widget.start_exercise_file.connect(self.show_exercise_window)
 
     def resizeEvent(self, event):
         self.widget_stack.resize(self.size())
@@ -32,6 +38,7 @@ class MainWindow(QWidget):
     def show_exercise_window(self, file: str):
         self.exercise_widget.set_file(file)
         self.exercise_widget.setup_exercise()
+        self.exercise_widget.start()
         self.widget_stack.setCurrentWidget(self.exercise_widget)
 
 
---
+++ epm/mainwidget.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+import os
+
+from PySide2.QtCore import Qt, Signal, Slot
+from PySide2.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget
+
+
+class MainWidget(QWidget):
+    start_exercise_file = Signal(str)
+
+    file_select_list_base = None
+    file_select_list = None
+
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.setup_widget()
+        self.load_exercise_files()
+
+    def setup_widget(self):
+        self.file_select_list_base = QWidget(self)
+        self.file_select_list = QVBoxLayout()
+        self.file_select_list.setAlignment(Qt.AlignTop)
+        self.file_select_list.setMargin(2)
+
+        self.file_select_list_base.setLayout(self.file_select_list)
+
+    def load_exercise_files(self):
+        path = str(Path.home()) + "/.local/share/epm"
+
+        if not Path(path).is_dir():
+            os.mkdir(path)
+
+        directories = os.listdir(path)
+
+        for dir in directories:
+            file = path + "/" + dir
+            widget = SelectPanel(file)
+            widget.setMinimumHeight(25)
+            widget.setMaximumHeight(25)
+            self.file_select_list.addWidget(widget)
+
+            widget.file_signal.connect(self.get_exercise_file)
+
+    def resizeEvent(self, event):
+        self.file_select_list_base.move(self.width() / 2 + 5, 5)
+        self.file_select_list_base.resize(self.width() / 2 - 10, self.height() - 10)
+
+    @Slot(str)
+    def get_exercise_file(self, file: str):
+        self.start_exercise_file.emit(file)
+
+
+class SelectPanel(QWidget):
+    name_label = None
+    start_button = None
+    edit_button = None
+
+    file_signal = Signal(str)
+
+    def __init__(self, file: str):
+        QWidget.__init__(self)
+
+        self.file = file
+
+        self.setup_widget()
+
+    def setup_widget(self):
+        with open(self.file, "r") as json_file:
+            data = json.load(json_file)
+
+        json_file.close()
+
+        self.name_label = QLabel(data['name'], self)
+        self.start_button = QPushButton("Start", self)
+        self.edit_button = QPushButton("Edit", self)
+
+        self.start_button.clicked.connect(self.send_file)
+
+    def resizeEvent(self, event):
+        self.name_label.resize(self.width() - 100, self.height())
+        self.start_button.resize(50, self.height())
+        self.edit_button.resize(50, self.height())
+
+        self.start_button.move(self.width() - 100, 0)
+        self.edit_button.move(self.width() - 50, 0)
+
+    @Slot()
+    def send_file(self):
+        self.file_signal.emit(self.file)
